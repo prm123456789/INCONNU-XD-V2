3c15b53eec5e344a4b2c19101207b52646c6b4e9
prm123456789 <guerrierjojo8@gmail.com>
2025-06-14 13:26:00 -0300

    Update generateAvatar.js
    Signed-off-by: prm123456789 <guerrierjojo8@gmail.com>

--- inconnu/generateAvatar.js
+++ inconnu/generateAvatar.js
@@ -1,1 +1,10 @@
 
+/**
+ * Génère un avatar DiceBear à partir d'un nom et d'un style
+ * @param {string} name - Le nom ou identifiant utilisé comme "seed"
+ * @param {string} style - Le style d'avatar (ex: "pixel", "bottts", "adventurer")
+ * @returns {string} URL de l’avatar SVG généré
+ */
+export const generateAvatar = (name = "inconnu", style = "adventurer") => {
+  return `https://api.dicebear.com/7.x/${encodeURIComponent(style)}/svg?seed=${encodeURIComponent(name)}`;
+};
